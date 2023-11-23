2d58ede519d4e634f45c38e5f961cdbb23a14a6c
Osher Solimany <solimani.osher@gmail.com>
2023-11-23 21:54:21 +0200

    Add basic express app

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require("dotenv").config();
+const express = require("express");
+
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Store API</h1>");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
