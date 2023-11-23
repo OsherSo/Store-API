03a32a06f6a14b670beb5cba29aea068ae770643
Osher Solimany <solimani.osher@gmail.com>
2023-11-23 22:30:21 +0200

    Add products router + basic getAllProducts controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/notFound");
+const productsRouter = require("./routes/products");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.get("/", (req, res, next) => {
   res.send("<h1>Store API</h1>");
 });
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ controllers/products.js
@@ -0,0 +1,7 @@
+const getAllProducts = async (req, res, next) => {
+  res.send("getAllProducts");
+};
+
+module.exports = {
+  getAllProducts,
+};
